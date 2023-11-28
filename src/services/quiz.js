bff6580de5f0c6a9f0db2b21ebf94597426db088
joejoubert123 <joe.joubert@gmail.com>
2023-11-28 12:58:54 +0000

    quiz name to uppercase

--- src/services/quiz.js
+++ src/services/quiz.js
@@ -43,7 +43,7 @@ async function getAllQuizzesAsync() {
   const list = quizzes.map((quiz) => {
     return {
       id: quiz.id,
-      name: quiz.name.toLowerCase(),
+      name: quiz.name.toUpperCase(),
     };
   });
   return list;
